51d2effe8814421da0259be645d54eea50b90110
Josiel <josielalv@hotmail.com>
2020-01-29 22:04:43 -0300

    Exercicio 1.2 logica de programação

--- ProvaPraticaLogicaProgramacao/ProvaPraticaLogicaProgramacao/Program.cs
+++ ProvaPraticaLogicaProgramacao/ProvaPraticaLogicaProgramacao/Program.cs
@@ -10,31 +10,43 @@ namespace ProvaPraticaLogicaProgramacao
 	{
 		static void Main(string[] args)
 		{
-			string codigo1;
-			int numero1;
-			decimal valor1;
-			string codigo2;
-			int numero2;
-			decimal valor2;
-			decimal valorPagar;
+			// EXERCICIO 1
+			//string codigo1;
+			//int numero1;
+			//decimal valor1;
+			//string codigo2;
+			//int numero2;
+			//decimal valor2;
+			//decimal valorPagar;
 
-			Console.Write("Código de uma peça 1: ");
-			codigo1 = Console.ReadLine();
-			Console.Write("Número de peças 1: ");
-			numero1 = int.Parse(Console.ReadLine());
-			Console.Write("Valor unitário peça 1: ");
-			valor1 = decimal.Parse(Console.ReadLine());
+			//Console.Write("Código de uma peça 1: ");
+			//codigo1 = Console.ReadLine();
+			//Console.Write("Número de peças 1: ");
+			//numero1 = int.Parse(Console.ReadLine());
+			//Console.Write("Valor unitário peça 1: ");
+			//valor1 = decimal.Parse(Console.ReadLine());
 
-			Console.Write("Código de uma peça 2: ");
-			codigo2 = Console.ReadLine();
-			Console.Write("Número de peças 2: ");
-			numero2 = int.Parse(Console.ReadLine());
-			Console.Write("Valor unitário peça 2: ");
-			valor2 = decimal.Parse(Console.ReadLine());
+			//Console.Write("Código de uma peça 2: ");
+			//codigo2 = Console.ReadLine();
+			//Console.Write("Número de peças 2: ");
+			//numero2 = int.Parse(Console.ReadLine());
+			//Console.Write("Valor unitário peça 2: ");
+			//valor2 = decimal.Parse(Console.ReadLine());
 
-			valorPagar = (numero1 * valor1) + (numero2 * valor2);
+			//valorPagar = (numero1 * valor1) + (numero2 * valor2);
 
-			Console.WriteLine($"VALOR A PAGAR: R$ {valorPagar}");
+			//Console.WriteLine($"VALOR A PAGAR: R$ {valorPagar}");
+
+			// EXERCICIO 2
+			decimal raio;
+			decimal area;
+			decimal pi = 3.14159M;
+
+			Console.Write("Raio: ");
+			raio = decimal.Parse(Console.ReadLine());
+
+			area = pi * (raio * raio);
+			Console.Write($"A={area.ToString("N4")}");
 
 			Console.Read();
 		}
