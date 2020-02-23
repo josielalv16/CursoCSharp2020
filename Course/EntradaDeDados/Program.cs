16b596fd7508641d3b2cfe5ac235f763e961bf00
Josiel <josielalv@hotmail.com>
2020-02-23 14:59:49 -0300

    Exercicios de Estrutura Sequencial

--- Course/EntradaDeDados/Program.cs
+++ Course/EntradaDeDados/Program.cs
@@ -24,24 +24,48 @@ namespace EntradaDeDados {
 			//Console.WriteLine(b);
 			//Console.WriteLine(c);
 
-			Console.WriteLine("===================== PARTE 2 =======================");
+			//Console.WriteLine("===================== PARTE 2 =======================");
 
-			int n1 = int.Parse(Console.ReadLine());
-			char ch = char.Parse(Console.ReadLine());
-			double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			//int n1 = int.Parse(Console.ReadLine());
+			//char ch = char.Parse(Console.ReadLine());
+			//double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+			//string[] vet = Console.ReadLine().Split(' ');
+			//string nome = vet[0];
+			//char sexo = char.Parse(vet[1]);
+			//int idade = int.Parse(vet[2]);
+			//double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+			//Console.WriteLine("Você digitou: ");
+			//Console.WriteLine(n1);
+			//Console.WriteLine(ch);
+			//Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+			//Console.WriteLine(nome);
+			//Console.WriteLine(sexo);
+			//Console.WriteLine(idade);
+			//Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+			Console.WriteLine("======================== Exercicio de fixação =========================");
+			
+			Console.WriteLine("Entre com seu nome completo:");
+			string nome = Console.ReadLine();
+
+			Console.WriteLine("Quantos quartos tem na sua casa?");
+			int quartos = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Entre com o preço de um produto:");
+			double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha):");
 			string[] vet = Console.ReadLine().Split(' ');
-			string nome = vet[0];
-			char sexo = char.Parse(vet[1]);
-			int idade = int.Parse(vet[2]);
-			double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+			string ultimoNome = vet[0];
+			int idade = int.Parse(vet[1]);
+			double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
 
-			Console.WriteLine("Você digitou: ");
-			Console.WriteLine(n1);
-			Console.WriteLine(ch);
-			Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
 			Console.WriteLine(nome);
-			Console.WriteLine(sexo);
+			Console.WriteLine(quartos);
+			Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+			Console.WriteLine(ultimoNome);
 			Console.WriteLine(idade);
 			Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
 		}
---
+++ Exercicios1EstruturaSequencial/Exercicios1EstruturaSequencial/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios1EstruturaSequencial {
+	class Program {
+		static void Main(string[] args) {
+
+			// Exercicio 01
+			int n1 = int.Parse(Console.ReadLine());
+			int n2 = int.Parse(Console.ReadLine());
+			int total = n1 + n2;
+
+			Console.WriteLine("SOMA = " + total);
+
+			Console.WriteLine("==============================");
+
+			// Exercicio 02
+			double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			double pi = 3.14159;
+			double area = pi * (Math.Pow(raio, 2));
+
+			Console.WriteLine("A=" + area.ToString("F4"));
+
+			Console.WriteLine("==============================");
+
+			// Exercicio 03
+			int a = int.Parse(Console.ReadLine());
+			int b = int.Parse(Console.ReadLine());
+			int c = int.Parse(Console.ReadLine());
+			int d = int.Parse(Console.ReadLine());
+
+			int dif = a * b - c * d;
+
+			Console.WriteLine("DIFERENCA = " + dif);
+
+			Console.WriteLine("==============================");
+
+			// Exercicio 04
+			int nFuncionario = int.Parse(Console.ReadLine());
+			int horasTrab = int.Parse(Console.ReadLine());
+			double vlrHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			double salario = horasTrab * vlrHora;
+
+			Console.WriteLine("NUMBER = " + nFuncionario);
+			Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+
+			Console.WriteLine("==============================");
+
+			// Exercicio 05
+			string[] p1 = Console.ReadLine().Split(' ');
+			int codigo1 = int.Parse(p1[0]);
+			int numero1 = int.Parse(p1[1]);
+			double vlr1 = double.Parse(p1[2], CultureInfo.InvariantCulture);
+
+			string[] p2 = Console.ReadLine().Split(' ');
+			int codigo2 = int.Parse(p2[0]);
+			int numero2 = int.Parse(p2[1]);
+			double vlr2 = double.Parse(p2[2], CultureInfo.InvariantCulture);
+
+			double vlrTotal = numero1 * vlr1 + numero2 * vlr2;
+
+			Console.WriteLine("Valor a Pagar: R$ " + vlrTotal.ToString("F2", CultureInfo.InvariantCulture));
+
+			Console.WriteLine("==============================");
+
+			// Exercicio 06
+			string[] valores = Console.ReadLine().Split(' ');
+			double a6 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+			double b6 = double.Parse(valores[1], CultureInfo.InvariantCulture);
+			double c6 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+			double triangulo = a6 * c6 / 2.0;
+			double circulo = 3.14159 * c6 * c6;
+			double trapezio = (a6 + b6) / 2.0 * c6;
+			double quadrado = b6 * b6;
+			double retangulo = a6 * b6;
+
+			Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+			Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+			Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+			Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+			Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+
+		}
+	}
+}
