02dbcccb576896ffbcf3a6d059a26cc826073252
Josiel <josielalv@hotmail.com>
2020-02-23 12:12:56 -0300

    Tipos Variaveis

---
+++ Course/Course/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Course {
+	class Program {
+		static void Main(string[] args) {
+
+			// Tipos de variaveis
+			sbyte x = 100;
+			byte n1 = 254;
+			int n2 = 1000;
+			int n3 = 2147483647;
+			long n4 = 2147483648;
+			n1++;
+
+			Console.WriteLine(n1);
+			Console.WriteLine(n2);
+			Console.WriteLine(n3);
+			Console.WriteLine(n4);
+			Console.WriteLine(x);
+		}
+	}
+}
