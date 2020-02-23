3be26298225d7d284389799e98fdc2c4d3cb628f
Josiel <josielalv@hotmail.com>
2020-02-23 13:20:56 -0300

    Operadores de Atribuição

---
+++ Course/OperadoresAtribuicao/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OperadoresAtribuicao {
+	class Program {
+		static void Main(string[] args) {
+
+			int a = 10;
+			Console.WriteLine(a);
+
+			a += 2;
+			Console.WriteLine(a);
+
+			a *= 3;
+			Console.WriteLine(a);
+
+			string s = "ABC";
+			Console.WriteLine(s);
+
+			s += "DEF";
+			Console.WriteLine(s);
+
+			int a1 = 10;
+			a1++;
+			Console.WriteLine(a1);
+			a1--;
+			Console.WriteLine(a1);
+
+			int b = a1++;
+			Console.WriteLine(a1);
+			Console.WriteLine(b);
+
+			b = ++a1;
+			Console.WriteLine(a1);
+			Console.WriteLine(b);
+
+
+		}
+	}
+}
